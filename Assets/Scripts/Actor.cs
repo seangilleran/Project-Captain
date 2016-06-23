70416c6b4c062363356a1e1fcbf26a586d40fb37
sean@wgworkshop.com <sean@wgworkshop.com>
2016-06-22 20:19:48 -0700

    Add SceneManager, Actor, and object selection

---
+++ Assets/Scripts/Actor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// This component is used by the SceneManager to determine which 
+/// objects are selected and which are not.
+/// </summary>
+class Actor : MonoBehaviour
+{
+    bool Selected;
+
+    void Start()
+    {
+        Selected = false;
+    }
+
+    public void Select()
+    {
+        Selected = true;
+        Debug.Log(this.name + ": I've been selected!");
+    }
+
+    public void Deselect()
+    {
+        Selected = false;
+    }
+}
---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The SceneManager should be keeping track of all Actors in play at 
+/// any given time in order to figure out which is selected. It should 
+/// also be responsible for conveying messages and state information 
+/// from Actor to Actor.
+/// </summary>
+class SceneManager : MonoBehaviour
+{
+    List<GameObject> Actors;
+    GameObject ActorSelected;
+    GameObject Player;
+
+    /// <summary>
+    /// Select an object in the scene. This will tell the object that
+    /// it is selected and deselect other objects. The object must 
+    /// handle its own logic after that.
+    /// </summary>
+    /// <param name="obj">Object to be selected</param>
+    GameObject SelectObject(GameObject obj)
+    {
+        if (!Actors.Contains(obj))
+        {
+            Debug.Log("SceneManager: Object is not an Actor and cannot be "
+                      + "selected.");
+            return null;
+        }
+
+        foreach (var actor in Actors)
+        {
+            actor.SendMessage("Deselect");
+        }
+        obj.SendMessage("Select");
+        return obj;
+    }
+
+    /// <summary>
+    /// Look for Actors and add them to the list. Find the actor 
+    /// representing the player and store that separately.
+    /// </summary>
+    void Start()
+    {
+        Actors = new List<GameObject>();
+        ActorSelected = null;
+        Player = null;
+
+        foreach (var obj in FindObjectsOfType<GameObject>())
+        {
+            if (obj.GetComponent<Actor>())
+            {
+                if (obj.tag == "Player")
+                {
+                    if (Player != null)
+                    {
+                        throw new System.Exception(
+                            "SceneManager: Too many player objects in Scene.");
+                    }
+                    Player = obj;
+                    continue;
+                }
+                Actors.Add(obj);
+            }
+        }
+
+        if (Actors.Count == 0)
+        {
+            throw new System.Exception("SceneManager: No Actors in Scene.");
+        }
+        if (Player == null)
+        {
+            throw new System.Exception("SceneManager: No player in Scene.");
+        }
+
+        Debug.Log("SceneManager: Found " + Actors.Count + " Actors in Scene:");
+        foreach (var actor in Actors)
+        {
+            Debug.Log("SceneManager: <" + actor.name + ">");
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonUp("Select"))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            var hit = new RaycastHit();
+            if (Physics.Raycast(ray, out hit))
+            {
+                var obj = hit.transform.gameObject;
+                if (Actors.Contains(obj))
+                {
+                    ActorSelected = this.SelectObject(obj);
+                }
+            }
+        }
+    }
+}
